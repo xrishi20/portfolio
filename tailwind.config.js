052a02b0537140fe84f82515bc375cd4ccab49a3
xrishi20 <xrishi20@gmail.com>
2024-01-12 20:28:32 +0530

    Tangy

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,22 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
-    extend: {},
+    colors: {
+      orange: {
+        DEFAULT: '#FF9869',
+        dark: '#FF661F'
+      },
+      white: '#FCFCF8',
+      black: '#010101',
+      purple: '#6A0DAD'
+    },
+    extend: {
+      fontFamily: {
+        lato: ['Lato', ...defaultTheme.fontFamily.sans]
+      }
+    },
   },
   plugins: [],
 };
